5a48247809483954a143bc43e7e4671f62df55fe
Federico Gutierrez <fedeguti2001@gmail.com>
2021-06-29 17:01:09 +0000

    initial commit

---
+++ sentiment.py
@@ -0,0 +1,10 @@
+import requests
+
+text = input('Enter a word: ')
+
+url = 'http://text-processing.com/api/sentiment'
+myobj = {'text': text}
+
+response = requests.post(url, data = myobj)
+
+print(response.json())
