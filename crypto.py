547ef5e637698953638fe571927e634a2e291298
Federico Gutierrez <fg42768n@pace.edu>
2021-06-30 16:27:34 -0400

    function implementation

--- crypto.py
+++ crypto.py
@@ -7,39 +7,63 @@ from sqlalchemy import create_engine
 rn_url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
 hist_url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
 
-response = requests.get(rn_url)
-r = response.json()
-price = r['bpi']
-dead_prez = price['USD']
-btc_cost = round(float(dead_prez['rate'].replace(',', '')))
 
-time = r['time']
-rn = time['updated']
-format_rn = rn + ','
+def query_current_price(url):
+    response = requests.get(url)
+    r = response.json()
+    price = r['bpi']
+    dead_prez = price['USD']
+    time = r['time']
+    rn = time['updated']
+    return (rn, round(float(dead_prez['rate'].replace(',', ''))))
 
-print('As of', format_rn, '1 Bitcoin currently costs $', btc_cost, 'USD!')
 
-response = requests.get(hist_url)
-r = response.json()
-prices = r['bpi']
+def query_historical_price(url):
+    response = requests.get(url)
+    r = response.json()
+    prices = r['bpi']
 
-clean_prices = tuple(prices.items())
-cleaner_prices = [(date[5:], round(price)) for date, price in clean_prices]
-cleanest_prices = cleaner_prices[21:]
+    clean_prices = tuple(prices.items())
+    cleaner_prices = [(date[5:], round(price)) for date, price in clean_prices]
+    return cleaner_prices[21:]
 
-zip(*cleanest_prices)
 
-chart_title = "BTC-USD Exchange Rate - Past 10 Days"
-print(chart_title)
-print("-" * len(chart_title))
+def plot_historical(prices):
+    zip(*prices)
 
-fig, ax = plt.subplots()  # Create a figure containing a single axes.
-ax.plot(*zip(*cleanest_prices))
+    chart_title = "BTC-USD Exchange Rate - Past 10 Days"
+    print(chart_title)
+    print("-" * len(chart_title))
 
-df = pd.DataFrame.from_records(cleanest_prices, columns=['Date', 'BTC-USD'])
+    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+    ax.plot(*zip(*prices))
+    fig.show()
 
-engine = create_engine('mysql://root:codio@localhost/crypto')
-df.to_sql('historical_btc', con=engine, if_exists='replace', index=False)
 
-print("Look at that price fluctuation!")
-print("Crypto is the future, but it's still in early stages.")
+def build_dataframe(pricelist):
+    return pd.DataFrame.from_records(pricelist, columns=['Date', 'BTC-USD'])
+
+
+def build_table(dataframe, name):
+    engine = create_engine('mysql://root:codio@localhost/crypto')
+    dataframe.to_sql(name, con=engine, if_exists='replace', index=False)
+
+
+def main():
+    query_time, btc_cost = query_current_price(rn_url)
+    cleanest_prices = query_historical_price(hist_url)
+    format_time = query_time + ','
+
+    print('As of', format_time, '1 BTC currently costs $', btc_cost, 'USD!')
+
+    plot_historical(cleanest_prices)
+
+    df = build_dataframe(cleanest_prices)
+    build_table(df, 'historical')
+
+    print("Look at that price fluctuation!")
+    print("Crypto is the future, but it's still in early stages.")
+
+
+if __name__ == "__main__":
+    main()
