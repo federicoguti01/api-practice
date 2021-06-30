51579c978b30bff3ee7768e9175f3933fd9b049c
Federico Gutierrez <fedeguti2001@gmail.com>
2021-06-30 19:44:50 +0000

    integrated MySQL

--- crypto.py
+++ crypto.py
@@ -1,15 +1,45 @@
 import requests
+import matplotlib.pyplot as plt
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
 
-url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+rn_url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+hist_url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
 
-response = requests.get(url)
+response = requests.get(rn_url)
 r = response.json()
 price = r['bpi']
 dead_prez = price['USD']
-btc_cost = dead_prez['rate']
+btc_cost = round(float(dead_prez['rate'].replace(',','')))
 
 time = r['time']
 rn = time['updated']
 format_rn = rn + ','
 
 print('As of', format_rn, '1 Bitcoin currently costs $', btc_cost, 'USD!')
+
+response = requests.get(hist_url)
+r = response.json()
+prices = r['bpi']
+
+clean_prices = tuple(prices.items())
+cleaner_prices = [(date[5:], round(price)) for date, price in clean_prices]
+cleanest_prices = cleaner_prices[21:]
+
+zip(*cleanest_prices)
+
+chart_title = "BTC-USD Exchange Rate - Past 10 Days"
+print(chart_title)
+print("-" * len(chart_title))
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot(*zip(*cleanest_prices))
+
+df = pd.DataFrame.from_records(cleanest_prices, columns=['Date', 'BTC-USD'])
+
+engine = create_engine('mysql://root:codio@localhost/crypto')
+df.to_sql('historical_btc', con=engine, if_exists='replace', index=False)
+
+print("Look at that price fluctuation!")
+print("Crypto is the future, but it's still in early stages.")
